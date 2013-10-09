64fbe52e6fd6ca42f0d6c1a172671bf9c2573dc5
tk0miya <i.tkomiya@gmail.com>
2013-10-09 12:35:02 +0900

    Use flake8 for code linting

--- setup.py
+++ setup.py
@@ -35,9 +35,8 @@ setup(
      ],
      extras_require=dict(
          test=[
-             'Nose',
-             'pep8',
              'mock',
+             'nose',
          ],
      ),
      test_suite='nose.collector',
