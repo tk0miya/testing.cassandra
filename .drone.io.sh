b0756174fb7201ba9fa5eb712a960e02ccc20977
tk0miya <i.tkomiya@gmail.com>
2014-03-01 15:26:32 +0900

    Update cassandra versions for testing

--- .drone.io.sh
+++ .drone.io.sh
@@ -19,25 +19,40 @@ cd /home/ubuntu/src/bitbucket.org/tk0miya/testing.cassandra
 pip install --use-mirrors --upgrade detox misspellings
 find src/ -name "*.py" | misspellings -f -
 detox
+rm -rf /usr/local/apache-cassandra-1.1.12
 
 
 ##################
 # Cassandra 1.2.x
 ##################
 cd /usr/local
-sudo curl -LO http://archive.apache.org/dist/cassandra/1.2.12/apache-cassandra-1.2.12-bin.tar.gz
-sudo tar xf apache-cassandra-1.2.12-bin.tar.gz
+sudo curl -LO http://archive.apache.org/dist/cassandra/1.2.15/apache-cassandra-1.2.15-bin.tar.gz
+sudo tar xf apache-cassandra-1.2.15-bin.tar.gz
 
 cd /home/ubuntu/src/bitbucket.org/tk0miya/testing.cassandra
 detox
+rm -rf /usr/local/apache-cassandra-1.2.15
 
 
 ##################
 # Cassandra 2.0.x
 ##################
 cd /usr/local
-sudo curl -LO http://archive.apache.org/dist/cassandra/2.0.3/apache-cassandra-2.0.3-bin.tar.gz
-sudo tar xf apache-cassandra-2.0.3-bin.tar.gz
+sudo curl -LO http://archive.apache.org/dist/cassandra/2.0.5/apache-cassandra-2.0.5-bin.tar.gz
+sudo tar xf apache-cassandra-2.0.5-bin.tar.gz
 
 cd /home/ubuntu/src/bitbucket.org/tk0miya/testing.cassandra
 detox
+rm -rf /usr/local/apache-cassandra-2.0.5
+
+
+##################
+# Cassandra 2.1.x
+##################
+cd /usr/local
+sudo curl -LO http://archive.apache.org/dist/cassandra/2.1.0/apache-cassandra-2.1.0-bin.tar.gz
+sudo tar xf apache-cassandra-2.1.0-bin.tar.gz
+
+cd /home/ubuntu/src/bitbucket.org/tk0miya/testing.cassandra
+detox
+rm -rf /usr/local/apache-cassandra-2.1.0
